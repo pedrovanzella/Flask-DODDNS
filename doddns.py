cfe729a02d9a1c0d88d96c7486073add07fca06d
Pedro Vanzella <pedro@pedrovanzella.com>
2015-01-23 15:38:45 -0200

    added auth

--- doddns.py
+++ doddns.py
@@ -1,12 +1,27 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+users = {
+    # IMPORTANT CHANGE THIS USERNAME AND PASSWORD
+    "user": "password"
+}
 
-@app.route("/")
-def hello():
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+@app.route("/<ip>")
+@auth.login_required
+def update_ip():
     return "Hello, World"
 
 
