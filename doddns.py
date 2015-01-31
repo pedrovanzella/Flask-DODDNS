3b3c1ca34efaa4a0ec8f91bea8ec7b260ddd57ae
Pedro Vanzella <pedro@pedrovanzella.com>
2015-01-31 12:49:51 -0200

    Avoid getting a sub.domain.com.sub.domain.com record by passing @ as a name

--- doddns.py
+++ doddns.py
@@ -53,7 +53,7 @@ def find_a_record_id(records):
 def edit_record(domain, record_id, ip):
     params = {}
     params['type'] = "A"
-    params['name'] = DOMAIN
+    params['name'] = "@"
     params['data'] = ip
     put('/domains/%s/records/%s' % (domain, record_id), params=params)
 
